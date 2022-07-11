da3e49b70571e6b0dd0a2cdf3a8e223ecb9f4f97
Maria Young <maria@agile.coop>
2022-07-11 13:44:35 +0100

    Updates tests for new field

--- tests/src/Functional/LocalGovPageTest.php
+++ tests/src/Functional/LocalGovPageTest.php
@@ -37,7 +37,7 @@ class LocalGovPageTest extends BrowserTestBase {
   protected static $modules = [
     'field_ui',
     'localgov_page',
-  ];
+    ];
 
   /**
    * {@inheritdoc}
@@ -60,7 +60,7 @@ class LocalGovPageTest extends BrowserTestBase {
     $this->drupalGet('/admin/structure/types/manage/localgov_page/fields');
     $this->assertSession()->statusCodeEquals(200);
     $this->assertSession()->pageTextContains('localgov_page_banner');
-    $this->assertSession()->pageTextContains('localgov_page_content');
+    $this->assertSession()->pageTextContains('localgov_paragraph_content');
     $this->assertSession()->pageTextContains('localgov_page_summary');
 
     // Test fields display.
